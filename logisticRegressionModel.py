d6da474d0006e5fbf228061a6b4a266034968df6
Alexander Wong <wongak626@gmail.com>
2019-04-08 09:02:25 -0400

    Add comments to python file

--- logisticRegressionModel.py
+++ logisticRegressionModel.py
@@ -1,34 +1,18 @@
-#importing the libraries
+# Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-#importing our cancer dataset
+# Importing our cancer dataset
 dataset = pd.read_csv('merged_for_modeling.csv')
 X = dataset.iloc[:, 1:8].values
 Y = dataset.iloc[:,9].values
 
-
-
-#print(X)
-# print(Y)
-#print(dataset.head())
-
-
-
-#Encoding categorical data values
-
-# from sklearn.preprocessing import LabelEncoder
-# labelencoder_Y = LabelEncoder()
-# Y = labelencoder_Y.fit_transform(Y)
-
-
-
-
+# Split the dataset into training and testing
 from sklearn.model_selection import train_test_split
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.25, random_state = 0)
 
-#Feature Scaling
+# Conduct feature Scaling
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
@@ -36,35 +20,29 @@ X_test = sc.transform(X_test)
 
 
 
-#Using Logistic Regression Algorithm to the Training Set
-
+# Use Logistic Regression Algorithm from sklearn on the Training Set
 from sklearn.linear_model import LogisticRegression
 classifier = LogisticRegression(random_state = 0)
 classifier.fit(X_train, Y_train)
 
 
-
+# Save predictions from testing data
 Y_pred = classifier.predict(X_test)
 
 Y_prob = classifier.predict_proba(X_test)
 preds = Y_prob[:,1]
 
 
-# print(len(Y_pred))
-print(Y_prob)
-print(preds)
-print(Y_test)
-
-
-
-
+# Create confusion matrix table
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(Y_test, Y_pred)
 
 print(cm)
 
+# Create ROC curve for prediction model
 from sklearn import metrics
 from sklearn.metrics import auc
+
 
 fpr, tpr, threshold = metrics.roc_curve(Y_test, preds,pos_label=2)
 roc_auc = metrics.auc(fpr,tpr)
